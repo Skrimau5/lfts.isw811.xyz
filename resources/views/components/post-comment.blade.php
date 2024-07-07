d47a16ca6fa4e2a6f17f12e403ec724c2ad4c185
Manuel Chavarria Sandoval <manuelchavarria528@gmail.com>
2024-07-07 17:45:02 -0600

    avance 52

--- resources/views/components/post-comment.blade.php
+++ resources/views/components/post-comment.blade.php
@@ -1,12 +1,3 @@
-[< Volver al Indice](/Docs/readme.md/)
-
-# Write the Markup for a Post Comment
-
-Vamos crear un nuevo archivo llamado `post-comment.blade.php` dentro de la carpeta components, que se encuentra dentro de la carpeta views.
-
-Una vez creado el archivo, agregamos el siguiente código para crear los comentarios en la pagina.
-
-```html
 <article class="flex bg-gray-100 border border-gray-200 p-6 rounded-xl space-x-4">
     <div class="flex-shrink-0">
         <img src="https://i.pravatar.cc/60" alt="" width="60" height="60" class="rounded-xl">
@@ -27,19 +18,3 @@ Una vez creado el archivo, agregamos el siguiente código para crear los comenta
         </p>
     </div>
 </article>
-```
-
-Luego nos vamos al archivo `show.blade.php` para hacer el llamado del archivo anteriormente creado. Este nuevo componente lo agregamos debajo del cierre del siguiente div: `<div class="col-span-8">`
-
-```html
-<section class="col-span-8 col-start-5 mt-10 space-y-6">
-    <x-post-comment />
-    <x-post-comment />
-    <x-post-comment />
-    <x-post-comment />
-</section>
-```
-
-Por ultimo revisamos los comentario en la pagina web.
-
-![Comments](./images/52.1%20comments.png)
